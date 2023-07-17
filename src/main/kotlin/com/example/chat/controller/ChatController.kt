b66149d4db7dc154d0ac8d0a8fc67cb8148aa879
namonak <namonak@gmail.com>
2023-07-18 01:13:17 +0900

    feat: add user session management
    - create a new user when a WebSocket session is connected
    - remove the user when the WebSocket session is disconnected
    - update handleMessage method to use the current session's user
    
    BREAKING CHANGE: The handleMessage method now uses the current session's user instead of a hardcoded user

--- src/main/kotlin/com/example/chat/controller/ChatController.kt
+++ src/main/kotlin/com/example/chat/controller/ChatController.kt
@@ -3,26 +3,39 @@ package com.example.chat.controller
 import com.example.chat.model.ChatMessage
 import com.example.chat.model.ChatRoom
 import com.example.chat.model.User
+import org.slf4j.LoggerFactory
+import org.springframework.context.event.EventListener
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.socket.messaging.SessionConnectEvent
+import org.springframework.web.socket.messaging.SessionDisconnectEvent
 import java.time.LocalDateTime
+import java.util.concurrent.ConcurrentHashMap
+
 
 @Controller
 class ChatController {
     private val chatRoom = ChatRoom(1, "General", null)
+    private val users: MutableMap<String, User> = ConcurrentHashMap()
+    private val logger = LoggerFactory.getLogger(ChatController::class.java)
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    fun handleMessage(message: String): ChatMessage {
-        val sender = User(1L, "John", "password") // Replace with actual sender information
-        val chatMessage = ChatMessage(1L, message, sender.username, LocalDateTime.now())
-
-        chatRoom.broadcastMessage(chatMessage)
-
-        return chatMessage
+    fun handleMessage(message: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage {
+        logger.info("Received message: $message")
+        val sessionId = headerAccessor.sessionId
+        val sender = users[headerAccessor.sessionId] // Get the user associated with the current session
+        if (sender != null) {
+            val chatMessage = ChatMessage(1L, message.content, sender.username, LocalDateTime.now())
+            chatRoom.broadcastMessage(chatMessage)
+            return chatMessage
+        }
+        throw RuntimeException("User not found for session id: $sessionId")
     }
 
     @GetMapping("/api/host-endpoint")
@@ -30,5 +43,31 @@ class ChatController {
     fun getHostEndpoint(): Map<String, String> {
         val hostEndpoint = System.getenv("HOST_ENDPOINT")
         return mapOf("hostEndpoint" to hostEndpoint)
+    }
+
+    @EventListener
+    fun handleSessionConnected(event: SessionConnectEvent) {
+        val accessor = StompHeaderAccessor.wrap(event.message)
+        val sessionId = accessor.sessionId
+        val user = User(1L, sessionId!!, "password") // Create a new user
+
+        users[sessionId] = user // Associate the user with the session
+
+        chatRoom.addParticipant(user) // Add the user to the chat room
+
+        logger.info("User ${user.username} connected")
+    }
+
+    @EventListener
+    fun handleSessionDisconnect(event: SessionDisconnectEvent) {
+        val accessor = StompHeaderAccessor.wrap(event.message)
+        val sessionId = accessor.sessionId
+        val user = users[sessionId] // Get the user associated with the session
+
+        if (user != null) {
+            chatRoom.removeParticipant(user) // Remove the user from the chat room
+            users.remove(sessionId) // Remove the user
+            logger.info("User ${user.username} disconnected")
+        }
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,48 +5,58 @@ const messageContainer = document.getElementById('message-container');
 let stompClient;  // 전역 범위로 선언
 
 // 환경 변수를 가져오는 API 호출
-fetch('/api/host-endpoint')
-    .then(response => response.json())
-    .then(data => {
-        const hostEndpointValue = data.hostEndpoint;
+fetch('/api/host-endpoint', {
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    const hostEndpointValue = data.hostEndpoint;
 
-        console.log('hostEndpointValue: ', hostEndpointValue);
+    console.log('hostEndpointValue: ', hostEndpointValue);
 
-        // SockJS 연결 설정
-        const socket = new SockJS(`${hostEndpointValue}/chat-websocket`);
+    // SockJS 연결 설정
+    const socket = new SockJS(`${hostEndpointValue}/chat-websocket`);
 
-        // Stomp 클라이언트 생성
-        stompClient = Stomp.over(socket);  // 초기화
+    // Stomp 클라이언트 생성
+    stompClient = Stomp.over(socket);  // 초기화
 
-        // 연결 성공 시 호출되는 콜백 함수
-        const onConnected = () => {
-            console.log('Connected to WebSocket');
+    // 연결 성공 시 호출되는 콜백 함수
+    const onConnected = () => {
+        console.log('Connected to WebSocket');
 
-            stompClient.subscribe('/topic/messages', (message) => {
-                //console.log('Received: ', receivedMessage);
-                const receivedMessage = JSON.parse(message.body);
-                const messageElement = document.createElement('div');
-                messageElement.classList.add('message');
-                messageElement.textContent = receivedMessage.sender + ': ' + receivedMessage.content;
-                messageContainer.appendChild(messageElement);
-            });
-        };
+        stompClient.subscribe('/topic/messages', (message) => {
+            //console.log('Received: ', receivedMessage);
+            const receivedMessage = JSON.parse(message.body);
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('message');
+            messageElement.textContent = receivedMessage.sender + ': ' + receivedMessage.content;
+            messageContainer.appendChild(messageElement);
+        });
+    };
 
-        // 에러 발생 시 호출되는 콜백 함수
-        const onError = (error) => {
-            console.error('Error: ', error);
-        };
+    // 에러 발생 시 호출되는 콜백 함수
+    const onError = (error) => {
+        console.error('Error: ', error);
+    };
 
-        // SockJS 연결 시도 및 Stomp 클라이언트 연결
-        stompClient.connect({}, onConnected, onError);
-    });
+    // SockJS 연결 시도 및 Stomp 클라이언트 연결
+    stompClient.connect({}, onConnected, onError);
+});
 
 // Send 버튼 클릭 이벤트 처리
 sendButton.addEventListener('click', () => {
     const message = messageInput.value;
     if (message && stompClient) {  // stompClient가 초기화된 상태인지 체크
         // 서버로 메시지 전송
-        stompClient.send('/app/chat', {}, message);
+        const messageObject = {
+            id: 1, // id 필드 추가
+            content: message,
+            sender: "DummySender",
+            timestamp: new Date().toISOString() // timestamp 필드 추가
+        };
+        stompClient.send('/app/chat', {}, JSON.stringify(messageObject));
         messageInput.value = '';
     }
 });
