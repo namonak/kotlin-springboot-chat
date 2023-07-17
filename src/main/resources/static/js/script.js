7ecfd6a4dd43590eced0ae21f18dc8510b584d38
namonak <namonak@gmail.com>
2023-07-18 01:23:15 +0900

    feat: add enter key event to send messages
    - extracted message sending logic into a separate function to avoid code duplication
    - added keypress event listener to the message input field
    - messages can now be sent by pressing the enter key (without shift)

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,6 +4,22 @@ const messageContainer = document.getElementById('message-container');
 
 let stompClient;  // 전역 범위로 선언
 
+// 메시지 전송 함수
+const sendMessage = () => {
+    const message = messageInput.value;
+    if (message && stompClient) {  // stompClient가 초기화된 상태인지 체크
+        // 서버로 메시지 전송
+        const messageObject = {
+            id: 1, // id 필드 추가
+            content: message,
+            sender: "DummySender",
+            timestamp: new Date().toISOString() // timestamp 필드 추가
+        };
+        stompClient.send('/app/chat', {}, JSON.stringify(messageObject));
+        messageInput.value = '';
+    }
+};
+
 // 환경 변수를 가져오는 API 호출
 fetch('/api/host-endpoint', {
     headers: {
@@ -46,17 +62,18 @@ fetch('/api/host-endpoint', {
 });
 
 // Send 버튼 클릭 이벤트 처리
-sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message && stompClient) {  // stompClient가 초기화된 상태인지 체크
-        // 서버로 메시지 전송
-        const messageObject = {
-            id: 1, // id 필드 추가
-            content: message,
-            sender: "DummySender",
-            timestamp: new Date().toISOString() // timestamp 필드 추가
-        };
-        stompClient.send('/app/chat', {}, JSON.stringify(messageObject));
-        messageInput.value = '';
+sendButton.addEventListener('click', sendMessage);
+
+// 메시지 입력 필드에 keypress 이벤트 리스너 추가
+messageInput.addEventListener('keypress', (event) => {
+    // 엔터 키가 눌렸는지 확인 (event.keyCode === 13)
+    // 이때, shift 키가 눌려진 상태가 아닌지도 확인 (event.shiftKey === false)
+    // shift 키가 눌려진 상태에서 엔터 키를 누르면 줄바꿈을 해야 하므로 메시지를 전송하지 않습니다
+    if (event.keyCode === 13 && event.shiftKey === false) {
+        // 기본 동작(여기서는 줄바꿈)을 취소
+        event.preventDefault();
+
+        // 메시지 전송 함수 호출
+        sendMessage();
     }
 });
