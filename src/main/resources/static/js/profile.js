4b4ea2487c8ddb9a022e004db8561c6d5c8be636
namonak <namonak@gmail.com>
2023-08-10 14:01:04 +0900

    feat(profile): add image validation to profile.js
    Validate the selected file to ensure it is a valid image. Check for allowed MIME types and size constraints, and use FileReader and Image objects to verify that the file is a loadable image.

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -24,29 +24,63 @@ window.onload = async function() {
   }
 };
 
-document.getElementById('profile-image').addEventListener('change', function(e) {
-  const file = e.target.files[0];
-  if (file) {
+function validateImage(file) {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      alert('이미지 파일을 업로드해주세요.');
+      return reject(false);
+    }
+
     const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
     if (!allowedMimeTypes.includes(file.type)) {
       alert('이미지 파일을 업로드해주세요. (jpeg, png, gif)');
-      e.target.value = ''; // 파일 입력 필드 초기화
-      document.getElementById('preview-image').style.display = 'none'; // 이미지 미리보기 영역 초기화
-      return;
+      return reject(false);
     }
+
     if (file.size > 1024 * 1024) { // 1MB
       alert('이미지 파일의 크기가 너무 큽니다. (최대 1MB)');
-      e.target.value = ''; // 파일 입력 필드 초기화
-      document.getElementById('preview-image').style.display = 'none'; // 이미지 미리보기 영역 초기화
-      return;
+      return reject(false);
     }
+
+    const image = new Image();
+    image.onerror = function() {
+      alert("잘못된 이미지 파일입니다.");
+      return reject(false);
+    };
+
     const reader = new FileReader();
     reader.onload = function(e) {
-      document.getElementById('preview-image').src = e.target.result;
-      document.getElementById('preview-image').style.display = 'block';
+      image.src = e.target.result; // 이미지 유효성 검사를 위해 먼저 Image 객체에 로드
+    };
+    reader.onerror = function() {
+      alert("파일을 읽는 데 오류가 발생했습니다.");
+      return reject(false);
     };
     reader.readAsDataURL(file);
-  }
+
+    image.onload = function() {
+      resolve(true); // 이미지가 정상적으로 로드되면 유효하다고 판단
+    };
+  });
+}
+
+document.getElementById('profile-image').addEventListener('change', function(e) {
+  const file = e.target.files[0];
+
+  validateImage(file).then(isValid => {
+    if (isValid) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        document.getElementById('preview-image').src = e.target.result;
+        document.getElementById('preview-image').style.display = 'block';
+      };
+      reader.readAsDataURL(file);
+    } else {
+      console.log('Invalid image file')
+      e.target.value = ''; // 파일 입력 필드 초기화
+      document.getElementById('preview-image').style.display = 'none'; // 이미지 미리보기 영역 초기화
+    }
+  });
 });
 
 document.getElementById('profile-form').addEventListener('submit', async (e) => {
@@ -55,48 +89,48 @@ document.getElementById('profile-form').addEventListener('submit', async (e) => 
   const fileInput = document.getElementById('profile-image');
   const file = fileInput.files[0];
 
-  if (file) {
-    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if (!allowedMimeTypes.includes(file.type)) {
-      alert('이미지 파일을 업로드해주세요. (jpeg, png, gif)');
-      return;
-    }
+  validateImage(file).then(async isValid => {
+    if (isValid) {
+      const user = supabase.auth.user();
+      const timestamp = Date.now();
+      const filePath = `ts_${timestamp}`;
+      const {error} = await supabase.storage.from('profile_image').upload(filePath, file, {upsert: true});
 
-    const user = supabase.auth.user();
-    const timestamp = Date.now();
-    const filePath = `ts_${timestamp}`;
-    const { error } = await supabase.storage.from('profile_image').upload(filePath, file, { upsert: true });
-
-    if (error) {
-      console.error('Error uploading image:', error);
-      alert('프로필 이미지 업로드에 실패하였습니다: ' + error.message)
-    } else {
-      console.log('Image uploaded successfully');
-
-      const userProfile = await getUserProfile(user.id);
-
-      // 서버에서 프로필 이미지 삭제
-      const deleteSuccess = await deleteProfileImage(user.id, userProfile.profile_image_name)
-      if (!deleteSuccess) {
-        console.error('Error deleting image');
-        alert('프로필 이미지 삭제에 실패하였습니다.');
-        return;
-      }
-
-      // 로컬 스토리지에서 프로필 이미지 URL 삭제
-      localStorage.removeItem(`${user.id}_profile_image`);
-
-      const updateSuccess = await updateProfileImageName(user.id, filePath);
-      if (updateSuccess) {
-        loadAndDisplayImage(user.id, file);
-        alert('프로필 이미지 변경이 성공적으로 완료되었습니다.')
-        window.location.href = '/chat.html'
+      if (error) {
+        console.error('Error uploading image:', error);
+        alert('프로필 이미지 업로드에 실패하였습니다: ' + error.message)
       } else {
-        console.error('Error: ', response.statusText)
-        alert('프로필 이미지 변경에 실패하였습니다: ' + response.statusText)
+        console.log('Image uploaded successfully');
+
+        const userProfile = await getUserProfile(user.id);
+
+        // 서버에서 프로필 이미지 삭제
+        const deleteSuccess = await deleteProfileImage(user.id, userProfile.profile_image_name)
+        if (!deleteSuccess) {
+          console.error('Error deleting image');
+          alert('프로필 이미지 삭제에 실패하였습니다.');
+          return;
+        }
+
+        // 로컬 스토리지에서 프로필 이미지 URL 삭제
+        localStorage.removeItem(`${user.id}_profile_image`);
+
+        const updateSuccess = await updateProfileImageName(user.id, filePath);
+        if (updateSuccess) {
+          loadAndDisplayImage(user.id, file);
+          alert('프로필 이미지 변경이 성공적으로 완료되었습니다.')
+          window.location.href = '/chat.html'
+        } else {
+          console.error('Error: ', response.statusText)
+          alert('프로필 이미지 변경에 실패하였습니다: ' + response.statusText)
+        }
       }
+    } else {
+      console.log('Invalid image file')
+      e.target.value = ''; // 파일 입력 필드 초기화
+      document.getElementById('preview-image').style.display = 'none'; // 이미지 미리보기 영역 초기화
     }
-  }
+  });
 });
 
 document.getElementById('back-button').addEventListener('click', function(e) {
